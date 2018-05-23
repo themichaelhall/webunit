cbcbd51f8bc34dc458c1e336fc1ff41a470b9d68
Michael Hall <hall@michaelhall.se>
2018-05-23 22:23:27 +0200

    Add ConsoleApplication class

---
+++ src/Application/ConsoleApplication.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * This file is a part of the webunit package.
+ *
+ * Read more at https://github.com/themichaelhall/webunit
+ */
+declare(strict_types=1);
+
+namespace MichaelHall\Webunit\Application;
+
+use DataTypes\Exceptions\FilePathInvalidArgumentException;
+use DataTypes\FilePath;
+use MichaelHall\PageFetcher\Interfaces\PageFetcherInterface;
+use MichaelHall\Webunit\Parser\Parser;
+
+/**
+ * Console application class.
+ *
+ * @since 1.0.0
+ */
+class ConsoleApplication
+{
+    /**
+     * Constructs the console application.
+     *
+     * @since 1.0.0
+     *
+     * @param int                  $argc        The command line argument count.
+     * @param string[]             $argv        The command line arguments.
+     * @param PageFetcherInterface $pageFetcher The page fetcher.
+     */
+    public function __construct(int $argc, array $argv, PageFetcherInterface $pageFetcher)
+    {
+        $this->argc = $argc;
+        $this->argv = $argv;
+        $this->pageFetcher = $pageFetcher;
+    }
+
+    /**
+     * Runs the console application.
+     *
+     * @since 1.0.0
+     *
+     * @return int The result code.
+     */
+    public function run(): int
+    {
+        echo "Webunit [dev] by Michael Hall." . PHP_EOL;
+
+        if ($this->argc !== 2) {
+            return self::fail('Usage: webunit testfile', 1);
+        }
+
+        $filePath = null;
+        try {
+            $filePath = FilePath::parse($this->argv[1]);
+        } catch (FilePathInvalidArgumentException $exception) {
+            return self::fail('Invalid file path "' . $this->argv[1] . '": ' . $exception->getMessage(), 2);
+        }
+
+        /** @noinspection PhpUsageOfSilenceOperatorInspection */
+        $content = @file($filePath->__toString());
+        if ($content === false) {
+            return self::fail('Could not open file "' . $filePath . '".', 2);
+        }
+
+        $parser = new Parser();
+        $parseResult = $parser->parse($filePath, $content);
+        if (!$parseResult->isSuccess()) {
+            foreach ($parseResult->getParseErrors() as $parseError) {
+                echo $parseError . PHP_EOL;
+            }
+
+            return self::fail('Parsing failed', 3);
+        }
+
+        $testResults = $parseResult->getTestSuite()->run($this->pageFetcher);
+        if (!$testResults->isSuccess()) {
+            foreach ($testResults->getFailedTestCaseResults() as $failedTestCaseResult) {
+                $failedTestCase = $failedTestCaseResult->getTestCase();
+                $failedAssertResult = $failedTestCaseResult->getFailedAssertResult();
+                $failedAssert = $failedAssertResult->getAssert();
+
+                echo $failedAssert->getLocation() . ': Test failed: ' . $failedTestCase->getUrl() . ': ' . $failedAssertResult->getError() . ".\n";
+            }
+
+            return self::fail('Tests failed', 4);
+        }
+
+        self::success('Tests completed successfully');
+
+        return 0;
+    }
+
+    /**
+     * Print a success message.
+     *
+     * @param string $message The message.
+     */
+    private static function success(string $message): void
+    {
+        echo "\033[42m\033[30m" . $message . "\033[0m" . PHP_EOL;
+    }
+
+    /**
+     * Prints an error message.
+     *
+     * @param string $message    The message.
+     * @param int    $resultCode The result code.
+     *
+     * @return int The result code.
+     */
+    private static function fail(string $message, int $resultCode): int
+    {
+        echo "\033[41m\033[1;37m" . $message . "\033[0m" . PHP_EOL;
+
+        return $resultCode;
+    }
+
+    /**
+     * @var int My command line argument count.
+     */
+    private $argc;
+
+    /**
+     * @var string[] My command line arguments.
+     */
+    private $argv;
+
+    /**
+     * @var PageFetcherInterface My page fetcher.
+     */
+    private $pageFetcher;
+}
---
+++ tests/Application/ConsoleApplicationTest.php
@@ -0,0 +1,183 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MichaelHall\Webunit\Tests\Application;
+
+use DataTypes\FilePath;
+use MichaelHall\PageFetcher\FakePageFetcher;
+use MichaelHall\PageFetcher\Interfaces\PageFetcherInterface;
+use MichaelHall\PageFetcher\Interfaces\PageFetcherRequestInterface;
+use MichaelHall\PageFetcher\Interfaces\PageFetcherResponseInterface;
+use MichaelHall\PageFetcher\PageFetcherResponse;
+use MichaelHall\Webunit\Application\ConsoleApplication;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test ConsoleApplication class.
+ */
+class ConsoleApplicationTest extends TestCase
+{
+    /**
+     * Test running application with missing file argument.
+     */
+    public function testMissingTestFileArgument()
+    {
+        $consoleApplication = new ConsoleApplication(1, ['webunit'], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        self::assertSame(1, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "\033[41m\033[1;37mUsage: webunit testfile\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Test running application with invalid file argument.
+     */
+    public function testInvalidTestFileArgument()
+    {
+        $consoleApplication = new ConsoleApplication(2, ['webunit', "Foo\0Bar"], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        self::assertSame(2, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "\033[41m\033[1;37mInvalid file path \"Foo\0Bar\": File path \"Foo\0Bar\" is invalid: Filename \"Foo\0Bar\" contains invalid character \"\0\".\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Test running application with missing test file.
+     */
+    public function testMissingTestFile()
+    {
+        $consoleApplication = new ConsoleApplication(2, ['webunit', __DIR__ . '/../Helpers/WebunitTests/missing.webunit'], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $filePath = FilePath::parse(__DIR__ . '/../Helpers/WebunitTests/missing.webunit');
+
+        self::assertSame(2, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "\033[41m\033[1;37mCould not open file \"{$filePath}\".\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Test running application with parse errors in test file.
+     */
+    public function testParseErrors()
+    {
+        $consoleApplication = new ConsoleApplication(2, ['webunit', __DIR__ . '/../Helpers/WebunitTests/parse-error.webunit'], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $filePath = FilePath::parse(__DIR__ . '/../Helpers/WebunitTests/parse-error.webunit');
+
+        self::assertSame(3, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "{$filePath}:4: Invalid argument: Invalid Url argument \"FooBar\" for \"GET\": Url \"FooBar\" is invalid: Scheme is missing." . PHP_EOL .
+            "{$filePath}:5: Syntax error: Invalid command \"Baz\"." . PHP_EOL .
+            "\033[41m\033[1;37mParsing failed\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Test running application with successful test suite.
+     */
+    public function testSuccessfulTestSuite()
+    {
+        $consoleApplication = new ConsoleApplication(2, ['webunit', __DIR__ . '/../Helpers/WebunitTests/success.webunit'], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        self::assertSame(0, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "\033[42m\033[30mTests completed successfully\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Test running application with failed test suite.
+     */
+    public function testFailedTestSuite()
+    {
+        $consoleApplication = new ConsoleApplication(2, ['webunit', __DIR__ . '/../Helpers/WebunitTests/fail.webunit'], $this->pageFetcher);
+
+        ob_start();
+        $result = $consoleApplication->run();
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        $filePath = FilePath::parse(__DIR__ . '/../Helpers/WebunitTests/fail.webunit');
+
+        self::assertSame(4, $result);
+        self::assertSame(
+            "Webunit [dev] by Michael Hall." . PHP_EOL .
+            "{$filePath}:3: Test failed: https://example.com/baz: Status code 404 was returned." . PHP_EOL .
+            "\033[41m\033[1;37mTests failed\033[0m" . PHP_EOL,
+            $output
+        );
+    }
+
+    /**
+     * Set up.
+     */
+    public function setUp()
+    {
+        $this->pageFetcher = new FakePageFetcher();
+
+        $this->pageFetcher->setResponseHandler(function (PageFetcherRequestInterface $request): PageFetcherResponseInterface {
+            switch ($request->getUrl()->getPath()) {
+                case '/':
+                    return new PageFetcherResponse(200, 'Hello World!');
+                case '/foo':
+                    return new PageFetcherResponse(200, 'This is Foo page.');
+                case '/bar':
+                    return new PageFetcherResponse(200, 'This is Bar page.');
+            }
+
+            return new PageFetcherResponse(404);
+        });
+    }
+
+    /**
+     * Tear down.
+     */
+    public function tearDown()
+    {
+        $this->pageFetcher = null;
+    }
+
+    /**
+     * @var PageFetcherInterface My page fetcher.
+     */
+    private $pageFetcher;
+}
